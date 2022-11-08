e68150b9484f1b58e94a8ed6f15cf4b51779855f
David05-max2 <jerardodromero@uniguajira.edu.co>
2022-11-08 10:00:38 -0500

    NAVBAR
    CREACION DE LA CLASE DE NAVBAR

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+
+import { NAVBAR } from './navbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+    <NAVBAR/>
   </React.StrictMode>
 )
