979894468839a3263e88833e90c2f9387f2ce619
David05-max2 <jerardodromero@uniguajira.edu.co>
2022-11-11 17:05:54 -0500

    actualizacion de acerca de

--- src/ui/components/Acercade.jsx
+++ src/ui/components/Acercade.jsx
@@ -8,10 +8,16 @@ export const ACERCADE = () => {
     <section className="acerca-de" id="acerca-de">
 			<div className="contenedor">
 				<div className="foto">
-				<img src='../../../LOGO/acer.jpg' alt="" />
+				<img src='../../../LOGO/acer.jpg' alt="imagen acerca de" />
 				</div>
 				<article>
-					<h3>Acerca de</h3>
+					{/* <div className='d-flex justify-content-center animate__animated animate__fadeInRight'>
+						
+							<img src="../../../LOGO/acercade.png" alt="logo" />
+						
+					</div> */}
+
+					<h3>NOSOTROS...</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 				
 					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
