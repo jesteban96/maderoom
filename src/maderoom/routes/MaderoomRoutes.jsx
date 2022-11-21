8cbd3ab54fc1366ff57ed1131db9115f337c9da4
JERARDO ROMERO <jerardodromero@uniguajira.edu.co>
2022-11-20 21:41:26 -0500

    docss

--- src/maderoom/routes/MaderoomRoutes.jsx
+++ src/maderoom/routes/MaderoomRoutes.jsx
@@ -1,18 +1,23 @@
 import { Route, Routes } from "react-router-dom"
+import { ACERCADE } from "../../ui/components/Acercade";
 import { Foother } from "../../ui/components/Foother";
 import { Navbar } from "../../ui/components/Navbar"
-import { Contactanos } from "../components";
+import { PROYECTOS } from "../../ui/components/Proyectos";
+import { Carrousel, Contactanos, Slogan } from "../components";
 import { MaderoomApp} from "../MaderoomApp";
 
 export const MaderoomRoutes = () => {
     return (
         <>
             <Navbar/>
-            
+            <Carrousel/>
+            <Slogan/>
+            <ACERCADE/>
+            <PROYECTOS/>
             <div >
 
                 <Routes>
-                    <Route path="" element={<MaderoomApp/>}/>
+                   {/*  <Route path="" element={<MaderoomApp/>}/> */}
                     <Route path="Contactanos" element={<Contactanos/>}/>
                 </Routes>
                 
