b0d96082b06c3a806f246ab233ae67ec9de2e1b6
Juan Esteban Fajardo Ruiz <drummerestebanruiz@gmail.com>
2022-11-20 22:49:25 -0500

    u

--- src/maderoom/routes/MaderoomRoutes.jsx
+++ src/maderoom/routes/MaderoomRoutes.jsx
@@ -2,8 +2,8 @@ import { Route, Routes } from "react-router-dom"
 import { ACERCADE } from "../../ui/components/Acercade";
 import { Foother } from "../../ui/components/Foother";
 import { Navbar } from "../../ui/components/Navbar"
-import { PROYECTOS } from "../../ui/components/Proyectos";
-import { Carrousel, Contactanos, Slogan } from "../components";
+// import { PROYECTOS } from "../../ui/components/Proyectos";
+// import { Carrousel, Contactanos, Slogan } from "../components";
 import { MaderoomApp} from "../MaderoomApp";
 
 export const MaderoomRoutes = () => {
@@ -17,7 +17,7 @@ export const MaderoomRoutes = () => {
             <div >
 
                 <Routes>
-                    <Route path="/" element={<MaderoomApp/>}/>
+                    <Route path="" element={<MaderoomApp/>}/>
                     {/* <Route path="Contactanos" element={<Contactanos/>}/> */}
                 </Routes>
                 
